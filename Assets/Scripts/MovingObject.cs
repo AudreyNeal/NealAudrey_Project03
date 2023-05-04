3d58ae9766635ee3293f106998e1ee9813c344a6
Audrey Neal <ann200006@utdallas.edu>
2023-05-04 06:32:56 -0500

    Loop and Back_Forth function added
    the platform can now loop and go back and forth once!

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -11,18 +11,21 @@ public class MovingObject : MonoBehaviour
   {
     LOOPING,
     BACK_AND_FORTH,
-    MOVE_ONCE,
-  }
+    ONE_WAY
+  };
+  public TypeOfMotion _typeOfMotion;
+
+
   [SerializeField] bool PlayOnAwake = true;
 
   [Header("Movement possitions")]
   // node system 
-  public int _numberOfNodes = 1;
 
   public Transform[] _nodes = new Transform [3];
   public float[] _waitTimes = new float [3];
 
-  private int _currentNode = 0; 
+  private int _currentNode = 0;
+  public bool _reverse = false;
   // allows for multiple nodes each with their own wait time
 
   [Header("Audio")]
@@ -44,29 +47,80 @@ public enum AudioPlayback
     // Update is called once per frame
     void Update()
     {
-        MoveObject();
     Debug.Log(_currentNode);
+    if (PlayOnAwake)
+    {
+      MoveObject();
+    }
+    
     }
 
     public void MoveObject()
   {
-  if (Vector3.Distance(transform.position, _nodes[_currentNode].position) < 0.1f)
+    if (Vector3.Distance(transform.position, _nodes[_currentNode].position) < 0.1f)
     {
+
       StartCoroutine(Wait(_speed));
-      _currentNode++;
-      /*
-      if (_currentNode >= _nodes.Length)
+      if (_typeOfMotion == TypeOfMotion.LOOPING)
       {
-        //return;
-        _currentNode --;
+        if (_currentNode == _nodes.Length - 1)
+        {
+          _reverse = true;
+
+        }
+        else if (_currentNode == 0) { _reverse = false; }
+
+        if (_reverse == true)
+        {
+          _currentNode--;
+        }
+        if (_reverse == false)
+        {
+          _currentNode++;
+        }
+      }
+      if (_typeOfMotion == TypeOfMotion.BACK_AND_FORTH) 
+      { 
+      bool CycleMade = false;
+        if (CycleMade == true) 
+        {
+          _currentNode = 0;
+        return; 
+        
+        }
+        
+
+        if (_reverse == true )
+        {
+          _currentNode--;
+        }
+        if (_reverse == false)
+        {
+          _currentNode++;
+        }
+        if (_currentNode == _nodes.Length - 1)
+        {
+          _reverse = true;
+
+        }
+     
+        if (_currentNode <= 0 && _reverse == true)
+        {
+        _currentNode = 0;
+          CycleMade= true;
+        }
+
+
 
       }
-      */
     }
 
-    transform.position = Vector3.MoveTowards(gameObject.transform.position, _nodes[_currentNode].position, _speed * Time.deltaTime);
+
+
+      transform.position = Vector3.MoveTowards(gameObject.transform.position, _nodes[_currentNode].position, _speed * Time.deltaTime);
 
   }
+
   private IEnumerator Wait (float currentSpeed) 
   {
     _speed = 0;
